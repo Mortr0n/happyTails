99ae7724ed00a8c03fad617084766956b48512bf
Mortron <cmsl4y3r@gmail.com>
2022-08-19 19:52:24 -0600

    working on responsive layout

--- happy-tails/src/components/NavBar.tsx
+++ happy-tails/src/components/NavBar.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import '../css/NavBar.css';
 import logo from '../images/HPTransparentTry1.png';
+import MenuIcon from '@mui/icons-material/Menu';
 
 const NavBar = () => {
     const navItems = [
@@ -10,33 +11,36 @@ const NavBar = () => {
         "Foster",
         "Spay",
         "Surrender",
-        "Donate",
         "Success",
-        "Posts"
+        "Posts",
     ]
 
 
 
     return(
-        <div className='nav-outer-div d-flex flex-row justify-content-between'>
-            <div className='nav-title-div nav-background'>
-                <NavLink className='link-title' to="/">
-                    <img className='logo' src={logo} alt='Happy Tails Logo' />
-                </NavLink>{" "}
+        <nav className='nav-outer-div navbar  d-flex flex-row justify-content-between'>
+            <div className='container'>
+                <div className='nav-title-div nav-background'>
+                    <NavLink className='link-title' to="/">
+                        <img className='logo' src={logo} alt='Happy Tails Logo' />
+                    </NavLink>{" "}
+                </div>
+                <div className='col-md-6 d-flex' id='list-div'>
+                    <ul className='nav-bar-ul nav-background d-flex flex-row justify-content-start'>
+                        {
+                            navItems.map((item) => {
+                                return(
+                                    <NavLink key={item} className={`link-item-${item} link-items`} to={`${item}`} ><li className='nav-items nav-background'>{item}</li></NavLink> 
+                                )
+                                
+                            })
+                        }
+                    </ul>
+                </div>
+                <MenuIcon fontSize='large' color="primary" className='menu-icon' />
             </div>
-            <div className='col-6'>
-                <ul className='nav-bar nav-background d-flex flex-row justify-content-start'>
-                    {
-                        navItems.map((item) => {
-                            return(
-                                <NavLink key={item} className={`link-item-${item} link-items`} to={`${item}`} ><li className='nav-items nav-background'>{item}</li></NavLink> 
-                            )
-                            
-                        })
-                    }
-                </ul>
-            </div>
-        </div>
+           
+        </nav>
         
     )
 }
--- happy-tails/src/views/Main.tsx
+++ happy-tails/src/views/Main.tsx
@@ -1,3 +1,7 @@
+import AccessAlarmIcon from '@mui/icons-material/AccessAlarm';
+import DeleteIcon from '@mui/icons-material/Delete';
+import MenuIcon from '@mui/icons-material/MenuOutlined'
+import Icon from '@mui/material/Icon';
 import React from "react";
 import '../css/Main.css'
 import cat from '../images/pexels-ion-ceban-ionelceban-6768704.jpg';
@@ -8,13 +12,18 @@ const Main  = () => {
 
     return(
         <>
-            <div className="container d-flex flex-row">
-                <div className="mt-4 col-6 d-flex justify-content-center">
-                    <p className="mission-statement">To save the hungry, the helpless, the lost, and the little ones just struggling to survive; to house and feed as many animals as possible; to ease the suffering of the weak and unloved.</p>
-                </div>
-                <div className="main-gallery-div d-flex offset-1 col-6">
+            <div className="container d-flex flex-row flex-shrink ">
+                <div className="mt-4 col-sm-10 d-flex justify-content-center d-flex">
+                    <div className='mission-statement-div'>
+                    <p className="mission-statement">To save the hungry, the helpless, the lost, and the little ones just struggling to survive; 
+                                    to house and feed as many animals as possible; to ease the suffering of the weak and unloved.</p>
+                    </div>
+                    
                     <img className="temp-image" src={cat} alt="temp cat picture" />
                 </div>
+                {/* <div className="main-gallery-div d-flex offset-1 col-sm-6 flex-shrink">
+                    
+                </div> */}
             </div>
         </>
     )
